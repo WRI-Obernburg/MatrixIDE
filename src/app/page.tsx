d6cffb304baf3dc23f12c0fa0fdb1755393a61d9
Tim Arnold <tim-arnold@outlook.de>
2025-01-15 11:58:34 +0100

    Added help dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
 "use client";
-import { Editor, useMonaco } from "@monaco-editor/react";
-import React, { useEffect } from "react";
-import { Button } from "@/components/ui/button";
+import {Editor, useMonaco} from "@monaco-editor/react";
+import React, {useEffect} from "react";
+import {Button} from "@/components/ui/button";
 import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
+    ResizableHandle, ResizablePanel, ResizablePanelGroup,
 } from "@/components/ui/resizable"
 import LoadExamples from "@/components/LoadExamples";
 import {compile} from "@/components/Compiler";
 import {CompilerDialog} from "@/components/CompilerDialog";
 import {useLocalStorage} from "usehooks-ts";
 import MatrixConnection from "@/components/MatrixConnection";
-
+import Image from "next/image";
+import HelpDialog from "@/components/HelpDialog";
 
 
 export default function Home() {
 
- const newGame = `
+    const newGame = `
     
 // Wird einmal beim Start deines Spiels aufgerufen
 function init() {
@@ -47,110 +46,148 @@ function on_event(event) {
 `
 
 
-  const [content, setContent] = useLocalStorage("content", newGame);
+    const [content, setContent] = useLocalStorage("content", newGame);
 
-  const [compilerOutput, setCompilerOutput] = React.useState<string>("");
-  const [compilerBlob, setCompilerBlob] = React.useState<Blob | null>(null);
-  const [compilingDone, setCompilingDone] = React.useState<boolean>(false);
+    const [compilerOutput, setCompilerOutput] = React.useState<string>("");
+    const [compilerBlob, setCompilerBlob] = React.useState<Blob | null>(null);
+    const [compilingDone, setCompilingDone] = React.useState<boolean>(false);
 
 
-  const monaco = useMonaco();
+    const monaco = useMonaco();
 
-  useEffect(() => {
-    if (monaco) {
-      monaco.languages.typescript.javascriptDefaults.setCompilerOptions({ noLib: true, allowNonTsExtensions: true });
-      monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({noSemanticValidation: true,
-        noSyntaxValidation: true,})
+    useEffect(() => {
+        if (monaco) {
+            monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+                noLib: true,
+                allowNonTsExtensions: true
+            });
+            monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+                noSemanticValidation: true, noSyntaxValidation: true,
+            })
+        }
+    }, [monaco]);
+
+    function download() {
+        const element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+        element.setAttribute('download', "program-code.matrixcode");
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
     }
-  }, [monaco]);
 
-  function download() {
-    const element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
-    element.setAttribute('download', "program-code.matrixcode");
+    function downloadCompiledProgram() {
+        const element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(compilerOutput));
+        element.setAttribute('download', "program.mb");
 
-    element.style.display = 'none';
-    document.body.appendChild(element);
+        element.style.display = 'none';
+        document.body.appendChild(element);
 
-    element.click();
+        element.click();
 
-    document.body.removeChild(element);
-  }
+        document.body.removeChild(element);
+    }
 
-  function downloadCompiledProgram() {
-    const element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(compilerOutput));
-    element.setAttribute('download', "program.mb");
+    function open() {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.onchange = () => {
+            // you can use this method to get file and perform respective operations
+            const files = Array.from(input.files!);
+            const reader = new FileReader();
+            reader.readAsText(files[0], "UTF-8");
+            reader.onload = function (evt) {
+                setContent(JSON.stringify(evt.target!.result!.toString()));
 
-    element.style.display = 'none';
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
-  }
-
-  function open() {
-    const input = document.createElement('input');
-    input.type = 'file';
-    input.onchange = () => {
-      // you can use this method to get file and perform respective operations
-      const files = Array.from(input.files!);
-      const reader = new FileReader();
-      reader.readAsText(files[0], "UTF-8");
-      reader.onload = function (evt) {
-        setContent( JSON.stringify(evt.target!.result!.toString()));
-
-      }
-    };
-    input.click();
-  }
-
-  return (
-    <ResizablePanelGroup direction="horizontal" className={"flex w-dvw h-dvh "}>
-      <ResizablePanel minSize={20} defaultSize={25}
-                      className={"h-100 flex-grow flex flex-col bg-card w-[30%] justify-between"}>
-        <div className={"font-bold self-center mt-4 text-2xl"}>Matrix-IDE</div>
-        <div className={"flex flex-col gap-4"}>
-
-          <CompilerDialog onClick={() => {
-            try {
-              const newCompilerOutput = compile(content);
-              setCompilerOutput(newCompilerOutput.string);
-              setCompilerBlob(newCompilerOutput.blob);
-              setCompilingDone(true);
-            }catch(e){
-              console.error(e);
-              setCompilingDone(false);
             }
-          }} downloadCompiledProgram={downloadCompiledProgram} showDownloadButton={compilingDone}/>
+        };
+        input.click();
+    }
+
+    return (<ResizablePanelGroup direction="horizontal" className={"flex w-dvw h-dvh "}>
+            <ResizablePanel minSize={20} defaultSize={25}
+                            className={"h-100 flex-grow flex flex-col bg-card w-[30%] justify-between"}>
+                <div className={"font-bold self-center mt-4 text-2xl"}>Matrix-IDE</div>
+                <div className={"flex flex-col gap-4"}>
+
+                    <CompilerDialog onClick={() => {
+                        try {
+                            const newCompilerOutput = compile(content);
+                            setCompilerOutput(newCompilerOutput.string);
+                            setCompilerBlob(newCompilerOutput.blob);
+                            setCompilingDone(true);
+                        } catch (e) {
+                            console.error(e);
+                            setCompilingDone(false);
+                        }
+                    }} downloadCompiledProgram={downloadCompiledProgram} showDownloadButton={compilingDone}/>
 
 
-          <MatrixConnection program={compilerBlob}/>
+                    <MatrixConnection program={compilerBlob}/>
 
-        </div>
+                </div>
+                <div className={"flex flex-col gap-6"}>
+                    <div className={"flex flex-row gap-2 self-center"}>
+                        <Button onClick={() => {
+                            setContent(newGame)
+                        }}>New</Button>
+                        <Button onClick={open}>Open</Button>
+                        <LoadExamples onProgramLoad={function (code: string): void {
+                            setContent(code);
+                        }}/>
+                        <Button onClick={download}>Download</Button>
+                    </div>
 
-        <div className={"flex flex-row gap-2 self-center mb-4"}>
-          <Button onClick={()=>{
-            setContent(newGame)
-          }}>New</Button>
-          <Button onClick={open}>Open</Button>
-          <LoadExamples onProgramLoad={function (code: string): void {
-            setContent(code);
-          }}/>
-          <Button onClick={download}>Download</Button>
-        </div>
+                    <footer
+                        className="row-start-3 flex gap-6 flex-wrap items-center justify-center mb-4">
+                        <HelpDialog/>
+                        <a
+                            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                            href="https://wri-obernburg.de"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                aria-hidden
+                                src="/window.svg"
+                                alt="Window icon"
+                                width={16}
+                                height={16}
+                            />
+                            WRI-Website
+                        </a>
+                        <a
+                            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                            href="https://github.com/wri-obernburg/MatrixIDE"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                aria-hidden
+                                src="/globe.svg"
+                                alt="Globe icon"
+                                width={16}
+                                height={16}
+                            />
+                            Github →
+                        </a>
+                    </footer>
+                </div>
 
-      </ResizablePanel>
-      <ResizableHandle withHandle/>
-      <ResizablePanel minSize={50}>
-        <Editor onChange={(content) => {
-          setContent(content ?? "");
-        }} value={content} language={"javascript"} theme={"vs-dark"} width={"100%"}>
+            </ResizablePanel>
+            <ResizableHandle withHandle/>
+            <ResizablePanel minSize={50}>
+                <Editor onChange={(content) => {
+                    setContent(content ?? "");
+                }} value={content} language={"javascript"} theme={"vs-dark"} width={"100%"}>
 
-        </Editor>
-      </ResizablePanel>
-    </ResizablePanelGroup>
-  );
+                </Editor>
+            </ResizablePanel>
+        </ResizablePanelGroup>);
 }
 
---
+++ src/components/HelpDialog.tsx
@@ -0,0 +1,46 @@
+"use client";
+import {
+    Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger,
+} from "@/components/ui/dialog"
+import {HelpCircle} from "lucide-react";
+import Link from "next/link";
+
+export default function HelpDialog() {
+    return <Dialog >
+        <DialogTrigger> <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="#"
+            rel="noopener noreferrer"
+        >
+            <HelpCircle
+                aria-hidden
+                width={16}
+                height={16}
+            />
+            Hilfe
+        </a></DialogTrigger>
+        <DialogContent className={"max-h-[60vh] max-w-[50vw] overflow-y-hidden"}>
+            <DialogHeader>
+                <DialogTitle className={"text-2xl"}>Informationen zur MatrixIDE</DialogTitle>
+                <DialogDescription className={"text-xl pt-4 flex justify-center"} asChild>
+
+                    <div className={"flex flex-col gap-6"}>
+                        <ul className={"flex gap-3 flex-col list-disc pl-5"}>
+                            <li>Die IDE kann genutzt werden, um eigene Spiele und Animation auf der LED Matrix zu realiseren
+                            </li>
+                            <li>Die Programmiersprache basiert auf <Link target={"_blank"} className={"underline hover:font-bold transition-all"} href={"https://home.workshopfriends.com/wrench/www/"}>Wrench</Link></li>
+                            <li>In Wrench sind die Erweiterungen Math, String und Container aktiv</li>
+                            <li>Vermeide es zu viel Speicher zu allokieren. Für deine Anwendung stehen insgesamt ca. 30 kB zur Verfügung</li>
+                            <li>Die Verbindung zur LED Matrix ist nur in Chromium Browsern möglich</li>
+                            <li>Liste aller <Link target={"_blank"} className={"underline hover:font-bold transition-all"} href={"https://github.com/WRI-Obernburg/PixelMatrix/blob/main/src/system/WrenchWrapper.h#L194"}>Matrix Funktionen</Link></li>
+                            <li><Link className={"underline hover:font-bold transition-all"} target={"_blank"} href={"/cheatsheet.pdf"}>Cheatsheet</Link></li>
+                        </ul>
+
+                    </div>
+
+
+                </DialogDescription>
+            </DialogHeader>
+        </DialogContent>
+    </Dialog>
+}
