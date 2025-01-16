ece447d92b568c3e038f4e4a2306fe51dda3fd69
Tim Arnold <tim-arnold@outlook.de>
2025-01-16 12:30:37 +0100

    improved autocomplete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,13 +58,41 @@ function on_event(event) {
 
     useEffect(() => {
         if (monaco) {
-            monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
-                noLib: true,
-                allowNonTsExtensions: true
+
+            monaco.languages.typescript.typescriptDefaults.setExtraLibs([{
+                content: `
+        function print(message: string) {}
+        function set_status(status: string) {}
+        function get_status(): string {}
+        function get_controls(): number {}
+        function set_controls(controls: number) {}
+        function get_current_tps(): number {}
+        function set_tps(tps: number) {}
+        function reset_controls() {}
+        function is_animation_running(): boolean {}
+        function stop_animation() {}
+        function set(x: number, y: number, color: number) {}
+        function off(x: number, y: number) {}
+        function fill(color: number) {}
+        function clear() {}
+        function line(x1: number, y1: number, x2: number, y2: number, color: number) {}
+        function rect(x: number, y: number, width: number, height: number, color: number) {}
+        function rect_filled(x: number, y: number, width: number, height: number, color: number) {}
+        function circle(x: number, y: number, radius: number, color: number) {}
+        function number(x: number, y: number, number: number, color: number) {}
+        function run_animation_splash(x: number, y: number, color: number, filled: number, duration: number, wait: number) {}
+        function random(min: number, max: number): number {}
+    `
+            }]);
+
+
+            monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+                noLib: true, allowNonTsExtensions: true,
             });
-            monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+            monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
                 noSemanticValidation: true, noSyntaxValidation: true,
             })
+
         }
     }, [monaco]);
 
@@ -110,87 +138,87 @@ function on_event(event) {
     }
 
     return (<ResizablePanelGroup direction="horizontal" className={"flex w-dvw h-dvh "}>
-            <ResizablePanel minSize={20} defaultSize={25}
-                            className={"h-100 flex-grow flex flex-col bg-card w-[30%] justify-between"}>
-                <div className={"font-bold self-center mt-4 text-2xl"}>Matrix-IDE</div>
-                <div className={"flex flex-col gap-4"}>
+        <ResizablePanel minSize={20} defaultSize={25}
+                        className={"h-100 flex-grow flex flex-col bg-card w-[30%] justify-between"}>
+            <div className={"font-bold self-center mt-4 text-2xl"}>Matrix-IDE</div>
+            <div className={"flex flex-col gap-4"}>
 
-                    <CompilerDialog changeMade={changeMade} onClick={() => {
-                        try {
-                            const newCompilerOutput = compile(content);
-                            setCompilerOutput(newCompilerOutput.string);
-                            setCompilerBlob(newCompilerOutput.blob);
-                            setCompilingDone(true);
-                        } catch (e) {
-                            console.error(e);
-                            setCompilingDone(false);
-                        }
-                        setChangeMade(false);
+                <CompilerDialog changeMade={changeMade} onClick={() => {
+                    try {
+                        const newCompilerOutput = compile(content);
+                        setCompilerOutput(newCompilerOutput.string);
+                        setCompilerBlob(newCompilerOutput.blob);
+                        setCompilingDone(true);
+                    } catch (e) {
+                        console.error(e);
+                        setCompilingDone(false);
+                    }
+                    setChangeMade(false);
 
-                    }} downloadCompiledProgram={downloadCompiledProgram} showDownloadButton={compilingDone}/>
+                }} downloadCompiledProgram={downloadCompiledProgram} showDownloadButton={compilingDone}/>
 
 
-                    <MatrixConnection changeMade={changeMade} program={compilerBlob}/>
+                <MatrixConnection changeMade={changeMade} program={compilerBlob}/>
 
-                </div>
-                <div className={"flex flex-col gap-6"}>
-                    <div className={"flex flex-row gap-2 self-center"}>
-                        <Button onClick={() => {
-                            setContent(newGame)
-                        }}>New</Button>
-                        <Button onClick={open}>Open</Button>
-                        <LoadExamples onProgramLoad={function (code: string): void {
-                            setContent(code);
-                        }}/>
-                        <Button onClick={download}>Download</Button>
-                    </div>
-
-                    <footer
-                        className="row-start-3 flex gap-6 flex-wrap items-center justify-center mb-4">
-                        <HelpDialog/>
-                        <a
-                            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                            href="https://wri-obernburg.de"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <Image
-                                aria-hidden
-                                src="/window.svg"
-                                alt="Window icon"
-                                width={16}
-                                height={16}
-                            />
-                            WRI-Website
-                        </a>
-                        <a
-                            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                            href="https://github.com/wri-obernburg/MatrixIDE"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <Image
-                                aria-hidden
-                                src="/globe.svg"
-                                alt="Globe icon"
-                                width={16}
-                                height={16}
-                            />
-                            Github →
-                        </a>
-                    </footer>
+            </div>
+            <div className={"flex flex-col gap-6"}>
+                <div className={"flex flex-row gap-2 self-center"}>
+                    <Button onClick={() => {
+                        setContent(newGame)
+                    }}>New</Button>
+                    <Button onClick={open}>Open</Button>
+                    <LoadExamples onProgramLoad={function (code: string): void {
+                        setContent(code);
+                    }}/>
+                    <Button onClick={download}>Download</Button>
                 </div>
 
-            </ResizablePanel>
-            <ResizableHandle withHandle/>
-            <ResizablePanel minSize={50}>
-                <Editor onChange={(content) => {
-                    setContent(content ?? "");
-                    setChangeMade(true);
-                }} value={content} language={"javascript"} theme={"vs-dark"} width={"100%"}>
+                <footer
+                    className="row-start-3 flex gap-6 flex-wrap items-center justify-center mb-4">
+                    <HelpDialog/>
+                    <a
+                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                        href="https://wri-obernburg.de"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Image
+                            aria-hidden
+                            src="/window.svg"
+                            alt="Window icon"
+                            width={16}
+                            height={16}
+                        />
+                        WRI-Website
+                    </a>
+                    <a
+                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                        href="https://github.com/wri-obernburg/MatrixIDE"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Image
+                            aria-hidden
+                            src="/globe.svg"
+                            alt="Globe icon"
+                            width={16}
+                            height={16}
+                        />
+                        Github →
+                    </a>
+                </footer>
+            </div>
 
-                </Editor>
-            </ResizablePanel>
-        </ResizablePanelGroup>);
+        </ResizablePanel>
+        <ResizableHandle withHandle/>
+        <ResizablePanel minSize={50}>
+            <Editor onChange={(content) => {
+                setContent(content ?? "");
+                setChangeMade(true);
+            }} value={content} language={"typescript"} theme={"vs-dark"} width={"100%"}>
+
+            </Editor>
+        </ResizablePanel>
+    </ResizablePanelGroup>);
 }
 
